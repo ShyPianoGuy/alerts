9c999046209d04f57183271705c365fadbb0cdae
ShyPianoGuy <devinjahr32@gmail.com>
2025-09-12 03:03:14 -0700

    we're gonna make this python instead for the fun of it

--- apps/api_handler/src/config.ts
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-
-export const config = {
-  secretKey: process.env.SECRET_KEY,
-  databaseUrl: process.env.DATABASE_URL,
-  port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
-};
--- apps/api_handler/src/routes/x.ts
+++
@@ -1,5 +0,0 @@
-import type { FastifyReply, FastifyRequest } from "fastify";
-
-export function handle(req: FastifyRequest, reply: FastifyReply) {
-  console.log('Twitch route initialized');
-}
